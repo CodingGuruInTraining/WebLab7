aaf594b27119b290a6d5e1b24cfad19d6b69e97d
mf <myemail@isawesome.com>
2017-02-23 20:59:49 -0600

    working on result

--- currency/routes/index.js
+++ currency/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var exchangeRates = { 'EUR' : 0.94, 'JPY' : 112.86 };
+var exchangeRates = { 'EUR' : 0.94, 'JPY' : 112.86, 'EP' : 0.063 };
 
 router.get('/', function(req, res){
     res.render('index');
@@ -14,14 +14,17 @@ router.get('/convert', function(req, res){
 
     var rateToDollar = exchangeRates[convertTo];
     var rateFromDollar = exchangeRates[convertFrom];
-    var result = input * rateToDollar;
 
-    if (rateToDollar < rateFromDollar) {
-        result = result * rateFromDollar;
-    }
-    else {
-        result = result / rateFromDollar;
-    }
+    var result = input / ((1/convertTo) * (1/convertFrom));
+
+    // var result = input * rateToDollar;
+    //
+    // if (rateToDollar < rateFromDollar) {
+    //     result = result * rateFromDollar;
+    // }
+    // else {
+    //     result = result / rateFromDollar;
+    // }
 
 
     res.render('results', { input : input, result: result, currencyTo: convertTo, currencyFrom: convertFrom})
