0cefae1eab1cee9f934c5131833cd382335f7b41
mf <myemail@isawesome.com>
2017-03-07 14:51:43 -0600

    cleanup; previous commit changed rates

--- currency/routes/index.js
+++ currency/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-// var exchangeRates = { 'EUR' : 0.94, 'JPY' : 112.86, 'EP' : 0.063 };
 var exchangeRates = { 'EUR' : 0.9458, 'JPY' : 113.938, 'EP' : 17.4983 };
 
 router.get('/', function(req, res){
@@ -16,43 +15,10 @@ router.get('/convert', function(req, res) {
     var rateTo = exchangeRates[convertTo];
     var rateFrom = exchangeRates[convertFrom];
 
-    var toDollars = input/rateFrom;         //  (input * rateFrom)/rateTo;
-    console.log("dollars: " + toDollars);
+    var toDollars = input/rateFrom;
     var fromDollars = toDollars * rateTo;
 
     var result = fromDollars;
-
-
-
-
-
-    // var fromAmount = input * rateFromDollar;
-
-    // if (rateFrom > 1) {
-    //
-    //     var dollarAmount = input / rateFrom;
-    // // var toAmount = fromAmount * rateToDollar;
-    // }
-    // else {
-    //     var dollarAmount = input * rateFrom;
-    // }
-    //
-    //
-    // // if (rateTo > 1) {
-    //     var result = dollarAmount * rateTo;
-    // // }
-
-        // input / ((1/rateToDollar) * (1/rateFromDollar));
-
-    // var result = input * rateToDollar;
-    //
-    // if (rateToDollar < rateFromDollar) {
-    //     result = result * rateFromDollar;
-    // }
-    // else {
-    //     result = result / rateFromDollar;
-    // }
-
 
     res.render('results', { input : input, result: result, currencyTo: convertTo, currencyFrom: convertFrom})
 });
