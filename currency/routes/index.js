f0306af4f13bc24b248378b612b5f6aab2412996
mf <myemail@isawesome.com>
2017-03-07 14:50:54 -0600

    Success!

--- currency/routes/index.js
+++ currency/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var exchangeRates = { 'EUR' : 0.94, 'JPY' : 112.86, 'EP' : 0.063 };
+// var exchangeRates = { 'EUR' : 0.94, 'JPY' : 112.86, 'EP' : 0.063 };
+var exchangeRates = { 'EUR' : 0.9458, 'JPY' : 113.938, 'EP' : 17.4983 };
 
 router.get('/', function(req, res){
     res.render('index');
@@ -12,29 +13,34 @@ router.get('/convert', function(req, res) {
     var convertTo = req.query.to_currency;
     var convertFrom = req.query.from_currency;
 
-    // if (convertTo == convertFrom) {
-    //     alert("Same currencies selected.")
-    // }
-
     var rateTo = exchangeRates[convertTo];
     var rateFrom = exchangeRates[convertFrom];
+
+    var toDollars = input/rateFrom;         //  (input * rateFrom)/rateTo;
+    console.log("dollars: " + toDollars);
+    var fromDollars = toDollars * rateTo;
+
+    var result = fromDollars;
+
+
+
 
 
     // var fromAmount = input * rateFromDollar;
 
-    if (rateFrom > 1) {
-
-        var dollarAmount = input / rateFrom;
-    // var toAmount = fromAmount * rateToDollar;
-    }
-    else {
-        var dollarAmount = input * rateFrom;
-    }
-
-
-    // if (rateTo > 1) {
-        var result = dollarAmount * rateTo;
+    // if (rateFrom > 1) {
+    //
+    //     var dollarAmount = input / rateFrom;
+    // // var toAmount = fromAmount * rateToDollar;
     // }
+    // else {
+    //     var dollarAmount = input * rateFrom;
+    // }
+    //
+    //
+    // // if (rateTo > 1) {
+    //     var result = dollarAmount * rateTo;
+    // // }
 
         // input / ((1/rateToDollar) * (1/rateFromDollar));
 
