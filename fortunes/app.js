5229e48b45427fb0f4e07859489cf9a7968da28f
mf <myemail@isawesome.com>
2017-03-07 15:39:34 -0600

    started working on advanced part. I decided to try keeping everything in the same project and see how it goes.

---
+++ fortunes/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    res.type('text/plain');
+    res.send('Fortunes R Us');
+});
+
+app.get('/about', function(req, res) {
+    res.type('text/plain');
+    res.send('All About Fortunes');
+});
+
+app.listen(app.get('port'), function() {
+   console.log('Express started!');
+});
