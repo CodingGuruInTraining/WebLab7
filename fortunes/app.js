760202f52c10f98f32e74f275ae24b4009d5c557
MF <notsure@compuserv.com>
2017-03-07 21:20:15 -0600

    tested everything worked. added comments and cleaned up some.

--- fortunes/app.js
+++ fortunes/app.js
@@ -1,12 +1,16 @@
+// Sets main objects.
 var express = require('express');
 var app = express();
-
 var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+
+// Sets engine.
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+// Sets middleware.
 app.use(express.static(__dirname + '/public'));
 
+// Sets designated port to use.
 app.set('port', process.env.PORT || 3000);
 
 // Route to home page.
@@ -16,13 +20,13 @@ app.get('/', function(req, res) {
 
 // Route to about page.
 app.get('/about', function(req, res) {
-    res.render('about');
+    // Generates a random array item.
     var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    console.log(randomFortune);
-    // Sets what goes in the 'fortune' placeholder.
+    // Sets randomly drawn array item into the 'fortune' placeholder.
     res.render('about', { fortune: randomFortune });
 });
 
+// Adds event listener to startup.
 app.listen(app.get('port'), function() {
    console.log('Express started!');
 });
