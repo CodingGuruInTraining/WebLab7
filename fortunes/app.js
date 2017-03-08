5347cf4f5bb85a9ca054d211727f3a521d5cfa50
MF <notsure@compuserv.com>
2017-03-07 20:41:49 -0600

    set up required things for home use and tested connections with both currency and fortunes projects.

--- fortunes/app.js
+++ fortunes/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
